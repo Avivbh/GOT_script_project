f0583bdaba5025e27e745cf0d1bd7f27d4e7aff9
Dvir <dvir.cohn@gmail.com>
2019-05-10 15:22:11 +0300

    added tv show embedding and embedding tester

--- embedding/embedding_tester.py
+++ embedding/embedding_tester.py
@@ -7,24 +7,38 @@ from gensim import utils
 from gensim.utils import SaveLoad
 
 
-model_path = "../data/w2v_models/book_v1_model"
+books_model_path = "../data/w2v_models/book_v1_model"
+tv_show_model_path = "../data/w2v_models/tv_show_v1_model"
 
 
-model = SaveLoad.load(model_path)
+books_model = SaveLoad.load(books_model_path)
+tv_show_model = SaveLoad.load(tv_show_model_path)
 
 
-pprint(model.similar_by_word("king"))
 
-pprint(model.similar_by_word("queen"))
+list_of_words = ['dragon', 'jon', 'cersei', 'arya']
 
-pprint(model.similar_by_word("dragons"))
-
-pprint(model.similar_by_word("khaleesi"))
-
-pprint(model.wv.most_similar(positive=['man', 'khaleesi'], negative=['woman']))
+words_triples_list = [['man','woman','king'],
+                      ['jaime', 'cersei', 'jon'],
+                      ['jaime', 'cersei', 'arya']
+                      ]
 
 
-pprint(model.wv.most_similar(positive=['jaime', 'jon'], negative=['cersei']))
+
+for word in list_of_words:
+    book_most_similar = books_model.wv.most_similar_cosmul(positive=word)[0][0]
+    tv_most_similar = tv_show_model.wv.most_similar_cosmul(positive=word)[0][0]
+    print("\nWord: {}. \n\tBooks most similar - {} \n\tTV show most similar - {}".format(
+        word, book_most_similar, tv_most_similar
+    ))
+
+for words_triples in words_triples_list:
+    book_most_similar = books_model.wv.most_similar_cosmul(positive=[words_triples[1], words_triples[2]], negative=[words_triples[0]])[0][0]
+    tv_most_similar = tv_show_model.wv.most_similar_cosmul(positive=[words_triples[1], words_triples[2]], negative=[words_triples[0]])[0][0]
+    print("\n{} is to {} like {} is to: \n\tBooks - {} \n\tTV show - {}".format(
+        words_triples[0],words_triples[1],words_triples[2], book_most_similar, tv_most_similar
+    ))
 
 pass
+
 
--- embedding/w2v_generetor.py
+++ embedding/w2v_generetor.py
@@ -1,48 +1,101 @@
+import pickle
+from nltk.stem.lancaster import LancasterStemmer
 import pandas as pd
 import numpy as np
 # from gensim.models import FastText
 import gensim
 from gensim.models import FastText, Word2Vec
 import string
-
-books_path = "../data/book_raw/all books2.txt"
-# books_path = "C:\\Users\\Dvir\\Desktop\\Desktop Icons\\Master\\NLP\\GOT books\\v3 1.AGameOfThrones-GeorgeR.R.Martin.txt"
-
-output_model_path = "../data/w2v_models/book_v1_model"
+from nltk.corpus import stopwords
 
 
-f = open(books_path, "r", encoding="utf8")
-print("reading file")
+
+def read_file(input_path):
+    f = open(input_path, "r", encoding="utf8")
+    print("reading file")
+    return f
+
+def read_tv_show_pkl_and_save_to_txt():
+
+    text = ""
+
+    for season in range(2,7):
+        tv_show_input_path = "../parsed/season{}.pkl".format(season)
+
+        f = open(tv_show_input_path, "rb")
+        scenes = pickle.load(f)
+
+        for scene in scenes:
+            for dialog in scene._script:
+                text += " " + dialog[1]
+
+    tv_out_path = "../data/tv_show_text_raw/seasons_2_to_7.txt"
+
+    f = open(tv_out_path, "w", encoding="utf8")
+    f.write(text)
+    f.close()
+
+def parse_text(file):
+    parsed_data = file.read() \
+        .replace('“', "") \
+        .replace('”', "") \
+        .replace('‘', "") \
+        .replace('’', "") \
+        .replace('\f', "") \
+        .replace('\n', " ") \
+        .replace('\t', " ") \
+        .replace(',', " ") \
+        .replace('?', "") \
+        .replace('…', "") \
+        .replace('—', "") \
+        .replace("'", "") \
+        .rstrip().split('.')
+
+    parsed_data = [x.translate(str.maketrans('', '', string.punctuation)) for x in parsed_data]
+
+    s = set(stopwords.words('english'))
+
+    parsed_data = [x.lower().strip().rstrip().split(' ') for x in parsed_data]
+
+    # parsed_data = [list(filter(lambda w: not w in s, sentence)) for sentence in parsed_data]
+    parsed_data = [list(filter(None, sentence)) for sentence in parsed_data]
 
 
-lines = f.read()\
-    .replace('“', "")\
-    .replace('”', "")\
-    .replace('‘', "")\
-    .replace('’', "")\
-    .replace('\f', "")\
-    .replace('\n', " ")\
-    .replace('\t', " ")\
-    .replace(',', " ") \
-    .replace('?', "")\
-    .replace('…', "")\
-    .replace('—', "")\
-    .replace("'", "")\
-    .rstrip().split('.')
+    print("done preproccesing")
 
-lines = [x.translate(str.maketrans('', '', string.punctuation)) for x in lines]
-lines = [x.lower().strip().rstrip().split(' ') for x in lines]
+    return parsed_data
 
 
-print("done preproccesing")
-pass
+def create_w2v_model(parsed_data):
+    model = Word2Vec(size=80, window=6, min_count=10, alpha=0.03)  # instantiate
+    model.build_vocab(sentences=parsed_data)
+    print("done building vocab")
+
+    model.train(sentences=parsed_data, total_examples=model.corpus_count, epochs=50)  # train
+    print("done training")
+    return model
 
 
-model = Word2Vec(size=50, window=4, min_count=1)  # instantiate
-model.build_vocab(sentences=lines)
-print("done building vocab")
+def save_model(model, output_model_path):
+    model.save(output_model_path)
+    print("model saved")
 
-model.train(sentences=lines, total_examples=len(lines), epochs=10)  # train
-print("done training")
-model.save(output_model_path)
-pass
+
+if __name__ == '__main__':
+    # input_path = "../data/book_raw/all books2.txt"
+    input_path = "../data/tv_show_text_raw/seasons_2_to_7.txt"
+    output_model_path = "../data/w2v_models/tv_show_v1_model"
+
+    is_books = False
+
+    # read_tv_show_pkl_and_save_to_txt()
+
+    file = read_file(input_path)
+
+    parsed_data = parse_text(file)
+
+    model = create_w2v_model(parsed_data)
+
+    save_model(model, output_model_path)
+
+
